70ef39b6b75f345cdb2fb248ccfa8ab131c21084
sramakrishnan247 <sramakrishnan247@gmail.com>
2020-11-15 15:25:40 -0600

    unit tested main.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String inputFile = "./src/main/resources/test1.txt";
+        String inputFile = "./src/main/resources/test0.txt";
+//        String inputFile = "./src/main/resources/test1.txt";
+//        String inputFile = "./src/main/resources/test2.txt";
+//        String inputFile = "./src/main/resources/test3.txt";
         AI ai = new AI();
         ai.prove(inputFile);
     }
